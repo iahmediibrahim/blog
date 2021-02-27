58c5ef70dde988bb5f9c913053b12ab84badcd10
swethusiast <aitc.dev@gmail.com>
2021-02-27 22:10:17 +0200

    latest

---
+++ src/page-renderer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+const generatePage = (page) => {
+    const component = () => require(`./pages/${page}`).default;
+    try {
+        return React.createElement(component());
+    } catch (err) {
+        console.warn(err);
+        return React.createElement(() => 404);
+    }
+};
+export default () => {
+    const { params: { page } } = useRouteMatch();
+    return generatePage(page);
+};
